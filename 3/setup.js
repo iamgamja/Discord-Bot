0f24632b37586d6d59818c3e78416eec6849d37d
iamgamja <hmmthinking@naver.com>
2022-03-19 17:20:15 +0900

    .

--- 3/setup.js
+++ 3/setup.js
@@ -1,4 +1,4 @@
 const {execSync} = require('child_process');
 
-execSync('py -m ensurepip --default-pip');
+execSync('python -m ensurepip --default-pip');
 execSync('pip install -r requriements.txt');
