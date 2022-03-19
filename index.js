379fd840668404892998e0d81f73499cca4817d0
iamgamja <hmmthinking@naver.com>
2022-03-19 15:54:28 +0900

    add test python

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const spawn = require("child_process").spawn;
 
 const process1 = spawn("node", ["/app/1/index.js"]);
 const process2 = spawn("node", ["/app/2/index.js"]);
+const process3 = spawn("python", ["/app/3/index.py"]);
 
 
 process1.stdout.on("data", function (data) {
   console.log(data.toString());
 });
 process2.stdout.on("data", function (data) {
+  console.log(data.toString());
+});
+process3.stdout.on("data", function (data) {
   console.log(data.toString());
 });
 
@@ -17,6 +21,10 @@ process1.stderr.on("data", function (data) {
   throw Error('kill');
 });
 process2.stderr.on("data", function (data) {
+  console.error(data.toString());
+  throw Error('kill');
+});
+process3.stderr.on("data", function (data) {
   console.error(data.toString());
   throw Error('kill');
 });
