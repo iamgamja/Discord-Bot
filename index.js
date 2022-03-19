03b19f118a82fc9684701a00bb3921c9e8d69342
iamgamja <hmmthinking@naver.com>
2022-03-19 16:18:14 +0900

    edit.

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
+function outCb(data) {
+  console.log(data.toString());
+}
+function errCb(data) {
+  console.error(data.toString());
+  throw Error('kill');
+}
+
 const spawn = require("child_process").spawn;
+const spawnPromiss = require('util').promisify(spawn);
 
 const process1 = spawn("node", ["/app/1/index.js"]);
+process1.stdout.on("data", outCb);
+process1.stderr.on("data", errCb);
+
 const process2 = spawn("node", ["/app/2/index.js"]);
-const process3 = spawn("python", ["/app/3/index.py"]);
+process2.stdout.on("data", outCb);
+process2.stderr.on("data", errCb);
 
-
-process1.stdout.on("data", function (data) {
-  console.log(data.toString());
-});
-process2.stdout.on("data", function (data) {
-  console.log(data.toString());
-});
-process3.stdout.on("data", function (data) {
-  console.log(data.toString());
-});
-
-
-process1.stderr.on("data", function (data) {
-  console.error(data.toString());
-  throw Error('kill');
-});
-process2.stderr.on("data", function (data) {
-  console.error(data.toString());
-  throw Error('kill');
-});
-process3.stderr.on("data", function (data) {
-  console.error(data.toString());
-  throw Error('kill');
-});
+(async () => {
+  await spawnPromiss('pip', ['install', '-r', 'requirements.txt']);
+  const process3 = spawn("python", ["/app/3/index.py"]);
+  process3.stdout.on("data", outCb);
+  process3.stderr.on("data", errCb);
+})();
 
 console.log('start');
