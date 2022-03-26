71b646e0a223592ff57107381305053d677b5070
iamgamja <hmmthinking@naver.com>
2022-03-26 17:04:47 +0900

    Update f4.js

--- 2/setup/TongGye/f4.js
+++ 2/setup/TongGye/f4.js
@@ -1,4 +1,9 @@
 function D2Date_(d){return d.setHours(d.getHours()+9),d}
+function sameDay(d1, d2) {
+  return d1.getFullYear() === d2.getFullYear() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getDate() === d2.getDate();
+}
 
 module.exports = async (client) => {
   client._.TongGye.f4 = async () => {
@@ -6,13 +11,13 @@ module.exports = async (client) => {
   
       const event = client.guilds.cache.get('843811221433352214').scheduledEvents.cache.find(e => {
         return e.name.endswith('생일') &&
-          D2Date_(e.scheduledStartAt).toDateString() === D2Date_(new Date()).toDateString();
+          sameDay(D2Date_(e.scheduledStartAt), D2Date_(new Date));
       });
 
       if (!event) {
         client.channels.cache.get('956573678638878730').setName('그리고 아무도 생일이지 않았다');
       } else {
-        client.channels.cache.get('956573678638878730').setName(`event.name.slice(0,-3) 생일`);
+        client.channels.cache.get('956573678638878730').setName(`${event.name.slice(0,-3)} 생일`);
       }
 
     } catch(e) { await client.Error(e) }
