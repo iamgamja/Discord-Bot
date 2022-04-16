2c59efda96c04541c9f6463462557ea7c17b7acd
iamgamja <hmmthinking@naver.com>
2022-04-16 15:20:25 +0900

    .

--- 1/commands/All/썸네일.js
+++ 1/commands/All/썸네일.js
@@ -19,15 +19,23 @@ exports.run = async ({client, message, Ms}) => {
   try {
 
     const result = await search(Ms[0], 1);
-    client.log(JSON.stringify(result.items[0].snippet));
 
-    await message.reply('wjfjs');
+    const item = result.items[0];
+    
+    const title = item.snippet.title;
+    const thumbnail1 = item.snippet.thumbnails.values().sort((a,b) => {
+      return a.width-b.width;
+    }).at(-1).url;
+    const id = item.id.videoId;
+    const thumbnail2 = `https://i.ytimg.com/vi/${id}/original.jpg`;
+
+    await message.reply(`${title}\n${thumbnail1}\n${thumbnail2}`);
     
   } catch (e) { await client.Error(e, message); }
 };
 
 exports.help = {
-  '<link>': '해당하는 영상의 썸네일을 출력합니다.'
+  '<검색어>': '검색된 영상의 썸네일을 출력합니다.'
 }
 exports.permission = ['All'];
 exports.MsLength = [1];
