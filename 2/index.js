d8189718ef78d5396708ca20970505eae2531804
iamgamja <hmmthinking@naver.com>
2022-04-23 16:44:42 +0900

    rename `TongGye` to `Loops`.

--- 2/index.js
+++ 2/index.js
@@ -3,7 +3,7 @@ const intents = new Discord.Intents(0b111111111111111);
 const client = new Discord.Client({ intents: intents });
 
 client._ = {};
-client._.TongGye = {};
+client._.Loops = {};
 
 (async () => {
   await require('/app/2/setup/log.js')(client);
@@ -12,11 +12,11 @@ client._.TongGye = {};
   await require('/app/2/setup/loadSlashCommands.js')(client);
   await require('/app/2/setup/db.js')(client);
 
-  await require('/app/2/setup/TongGye/f1.js')(client);
-  await require('/app/2/setup/TongGye/f2.js')(client);
-  await require('/app/2/setup/TongGye/f3.js')(client);
-  await require('/app/2/setup/TongGye/f4.js')(client);
-  await require('/app/2/setup/TongGye/index.js')(client);
+  await require('/app/2/setup/Loops/f1.js')(client);
+  await require('/app/2/setup/Loops/f2.js')(client);
+  await require('/app/2/setup/Loops/f3.js')(client);
+  await require('/app/2/setup/Loops/f4.js')(client);
+  await require('/app/2/setup/Loops/index.js')(client);
 
   // Event
   await require('/app/2/setup/event/interactionCreate.js')(client);
--- 2/setup/Loops/index.js
+++ 2/setup/Loops/index.js
@@ -5,22 +5,22 @@ function Date_(){const d=new Date;return d.setHours(d.getHours()+9),d}
 const Discord = require("discord.js");
 
 module.exports = async (client) => {
-  client.TongGye = async () => {
+  client.Loops = async () => {
     try {
   
       const date = Date_();
       date.setHours(date.getHours()-1);
       const hour = date.getHours(); // (hour) 시부터 (hour+1) 시까지
   
-      const A = await client._.TongGye.f1(); // array, 유저·봇 상태·수
-      const B = await client._.TongGye.f2(); // array, 메시지 수
+      const A = await client._.Loops.f1(); // array, 유저·봇 상태·수
+      const B = await client._.Loops.f2(); // array, 메시지 수
       const AB = [...A, ...B];               // array
       await client.db.add(['TongGye', 'B', hour], B[0]); // db에 메시지수 넣기
   
       if (hour === 23) {
-        client._.TongGye.f4();
+        client._.Loops.f4(); // 생일
 
-        const C = await client._.TongGye.f3(); // buffer, 그래프 이미지
+        const C = await client._.Loops.f3(); // buffer, 그래프 이미지
         await client.channels.cache.get('920536110382329907').send({ files: [new Discord.MessageAttachment(C, 'chart.png')] });
   
         await client.db.add(['TongGye', 'B'], new Array(24).fill(0)); // 재설정
